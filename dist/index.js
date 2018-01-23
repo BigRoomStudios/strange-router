d99ed92d3ce646b1c0bb042c49450ecf6166c9a4
William S. Woodruff <williamswoodruff@gmail.com>
2018-01-23 18:57:55 -0500

    WIP working on flattening routes

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,16 @@
 'use strict';
 
+var _toConsumableArray2 = require('babel-runtime/helpers/toConsumableArray');
+
+var _toConsumableArray3 = _interopRequireDefault(_toConsumableArray2);
+
 var _extends2 = require('babel-runtime/helpers/extends');
 
 var _extends3 = _interopRequireDefault(_extends2);
+
+var _assign = require('babel-runtime/core-js/object/assign');
+
+var _assign2 = _interopRequireDefault(_assign);
 
 var _getPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of');
 
@@ -75,15 +83,15 @@ exports.Router = (_temp = _class = function (_React$PureComponent) {
     routes: T.array.isRequired
 }, _temp);
 
-internals.routeComponentWrapper = (_temp2 = _class2 = function (_React$PureComponent2) {
-    (0, _inherits3.default)(RouteWrapper, _React$PureComponent2);
+internals.routeComponentLifecycleWrapper = (_temp2 = _class2 = function (_React$PureComponent2) {
+    (0, _inherits3.default)(RouteComponentLifecycleWrapper, _React$PureComponent2);
 
-    function RouteWrapper() {
-        (0, _classCallCheck3.default)(this, RouteWrapper);
-        return (0, _possibleConstructorReturn3.default)(this, (RouteWrapper.__proto__ || (0, _getPrototypeOf2.default)(RouteWrapper)).apply(this, arguments));
+    function RouteComponentLifecycleWrapper() {
+        (0, _classCallCheck3.default)(this, RouteComponentLifecycleWrapper);
+        return (0, _possibleConstructorReturn3.default)(this, (RouteComponentLifecycleWrapper.__proto__ || (0, _getPrototypeOf2.default)(RouteComponentLifecycleWrapper)).apply(this, arguments));
     }
 
-    (0, _createClass3.default)(RouteWrapper, [{
+    (0, _createClass3.default)(RouteComponentLifecycleWrapper, [{
         key: 'componentWillMount',
         value: function componentWillMount() {
             var _props2 = this.props,
@@ -132,7 +140,7 @@ internals.routeComponentWrapper = (_temp2 = _class2 = function (_React$PureCompo
             return this.props.children;
         }
     }]);
-    return RouteWrapper;
+    return RouteComponentLifecycleWrapper;
 }(React.PureComponent), _class2.propTypes = {
     match: T.object,
     location: T.object,
@@ -140,50 +148,171 @@ internals.routeComponentWrapper = (_temp2 = _class2 = function (_React$PureCompo
     route: T.object
 }, _temp2);
 
+internals.getRoutesWithParams = function (routes) {
+
+    var routesWithParams = [];
+    var routesWithoutParams = [];
+
+    routes.forEach(function (childRoute) {
+
+        // Params are denoted by `:`
+        if (childRoute.path && childRoute.path.includes(':')) {
+            routesWithParams.push(childRoute);
+        } else {
+            routesWithoutParams.push(childRoute);
+        }
+    });
+
+    return { routesWithParams: routesWithParams, routesWithoutParams: routesWithoutParams };
+};
+
+internals.absolutizePath = function (pathPrefix, route) {
+
+    // Remove any double slashes and we should be good!
+    var path = (pathPrefix + '/' + route.path).replace(/\/+/g, '/');
+
+    var clone = (0, _assign2.default)({}, route);
+
+    clone.path = path;
+
+    var boundAbsolutizePathFunc = internals.absolutizePath.bind(null, path);
+
+    if (clone.childRoutes) {
+        clone.childRoutes = clone.childRoutes.map(boundAbsolutizePathFunc);
+    }
+
+    return clone;
+};
+
+internals.buildRoutesFromAbsolutePaths = function () {
+
+    // Build backwards, and split by slashes
+};
+
+internals.reformatRoutes = function (routes) {
+
+    // For react-router v4, routes with params (:myId for example)
+    // will only match if they are siblings to their parent routes.
+
+    // The parent routes will also need to be set to exact: true
+    // in order for these to match
+
+    // This may have something to do with the `Switch` component
+    // or just how the v4 router is setup, I'm unsure at the moment
+
+    if (route.childRoutes) {
+        var _internals$getRoutesW = internals.getRoutesWithParams(route.childRoutes),
+            routesWithParams = _internals$getRoutesW.routesWithParams,
+            routesWithoutParams = _internals$getRoutesW.routesWithoutParams;
+
+        console.log('routesWithParams, routesWithoutParams', routesWithParams, routesWithoutParams);
+
+        // route.childRoutes.forEach((childRoute) => {
+        //
+        //     // Params are denoted by `:`
+        //     if (childRoute.path && childRoute.path.includes(':')) {
+        //         childrenWithParams.push(childRoute);
+        //     }
+        //     else {
+        //         finalChildRoutes.push(childRoute);
+        //     }
+        // });
+        //
+        // if (parentRoute) {
+        //     parentRoute.addedRoutes = childrenWithParams;
+        // }
+    }
+};
+
+internals.renderRoute = function (pathPrefix, route) {
+
+    // Remove any double slashes and we should be good!
+    var path = ('/' + pathPrefix + '/' + route.path).replace(/\/+/g, '/');
+
+    var boundPathPrefixRenderFunc = internals.renderRoute.bind(null, path);
+
+    if (!route.component) {
+        // Fail with a useful error msg & info to help debugging
+        console.log(route);
+        throw new Error('^^^^Component is falsy for route ' + path + ' logged above^^^^');
+    }
+
+    return React.createElement(Route, {
+        exact: route.exact,
+        key: path,
+        path: path,
+        strict: route.strict,
+        render: function render(props) {
+
+            return React.createElement(
+                internals.routeComponentLifecycleWrapper,
+                (0, _extends3.default)({}, props, { route: route }),
+                React.createElement(
+                    route.component,
+                    (0, _extends3.default)({}, props, { route: route }),
+                    route.childRoutes && route.childRoutes.length && React.createElement(
+                        Switch,
+                        null,
+                        route.childRoutes.map(boundPathPrefixRenderFunc)
+                    )
+                )
+            );
+        }
+    });
+};
+
 internals.renderRoutes = function (routes) {
 
-    var renderRouteRecursive = function renderRouteRecursive(pathPrefix, route) {
+    var absolutizedRoutes = routes.map(internals.absolutizePath.bind(null, '/'));
 
-        // These relative paths seem to require a slash in front
+    var flattenChildRoutes = function flattenChildRoutes(route) {
 
-        // Remove any double slashes and we should be good!
-        var path = (pathPrefix + '/' + route.path).replace(/\/+/g, '/');
+        // Clone because we're building an army &&
+        // Transform to an array so we can play
+        var clone = [].concat((0, _assign2.default)({}, route));
 
-        console.log('pathPrefix', pathPrefix);
-        console.log('path', path);
+        var parentRoute = clone[0];
 
-        var isLeafRoute = true;
+        if (parentRoute.childRoutes) {
 
-        if (!route.childRoutes) {
-            isLeafRoute = true;
+            var childRoutes = [].concat(parentRoute.childRoutes);
+            delete parentRoute.childRoutes;
+
+            return clone.concat(childRoutes.map(flattenChildRoutes));
         }
 
-        var boundRenderFunc = renderRouteRecursive.bind(null, path);
-
-        return React.createElement(Route, {
-            exact: route.exact,
-            key: path,
-            path: path,
-            strict: route.strict,
-            render: function render(props) {
-                return React.createElement(
-                    internals.routeComponentWrapper,
-                    (0, _extends3.default)({}, props, { route: route }),
-                    React.createElement(
-                        route.component,
-                        (0, _extends3.default)({}, props, { route: route }),
-                        route.childRoutes && route.childRoutes.length && React.createElement(
-                            Switch,
-                            null,
-                            route.childRoutes.map(boundRenderFunc)
-                        )
-                    )
-                );
-            }
-        });
+        return clone;
     };
 
-    var boundRenderFunc = renderRouteRecursive.bind(null, '/');
+    var flattenedChildRoutes = absolutizedRoutes.map(flattenChildRoutes);
+
+    var flattenArray = function flattenArray(arr) {
+
+        var flat = [];
+
+        arr.forEach(function (arrItem) {
+
+            if (Array.isArray(arrItem)) {
+
+                flat = flat.concat([].concat((0, _toConsumableArray3.default)(flattenArray(arrItem))));
+            } else {
+                flat.push(arrItem);
+            }
+        });
+
+        return flat;
+    };
+
+    console.log('flattenedChildRoutes', flattenedChildRoutes);
+
+    console.log('flatty', flattenArray(flattenedChildRoutes));
+
+    // console.log(internals.absolutizePath('/', routes[0]));
+
+    // const boundReformatFunc = internals.reformatRoutes.bind(null, null);
+    // console.log(routes.map(boundReformatFunc));
+
+    var boundRenderFunc = internals.renderRoute.bind(null, '/');
 
     return React.createElement(
         Switch,
