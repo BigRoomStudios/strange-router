eea245d290307f03accb462cc2dafbfe18d2e714
WilliamSWoodruff <williamswoodruff@gmail.com>
2018-05-10 10:49:38 -0400

    Some final adjustments to redirect

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # strange-router
-A module to aid bringing configurable routes to react-router v4.x
+
+A module to aid bringing configurable routes to react-router v4
+
+Lead Maintainer - [William Woodruff](https://github.com/wswoodruff)
+
+## Usage
+
+```bash
+npm install strange-router
+```
+
+### Config
+
+Configs have a recursive structure via `childRoutes`
+
+Props:
+
+`path`
+  - Required if `redirect` is not set
+  - Nested paths (via childRoutes) are prepended by their parent paths
+
+`component`
+  - Optional
+
+`childRoutes`
+  - Optional
+
+`redirect`
+  - Optional
+
+Ex:
+
+```js
+const routes = [
+    {
+        path: 'first', // matches '/first'
+        component: MyComponent,
+        childRoutes: [
+            {
+                path: 'second', // matches '/first/second'
+                childRoutes: [
+                    { path: 'third', // matches '/first/second/third'
+                        redirect: {
+                            to: '/404'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        path: '404',
+        component: FourOFour
+    }
+];
+```
+
+### Integration
+```js
+const React = require('react');
+const StrangeRouter = require('strange-router');
+const { ConnectedRouter } = require('react-router-redux');
+
+class App extends React.Component {
+
+    static propTypes = {
+        history: T.object.isRequired,
+        routes: T.array.isRequired,
+        store: T.object.isRequired
+    }
+
+    render() {
+
+        const { history, routes, store } = this.props;
+
+        return (
+            <Provider store={store}>
+                <ConnectedRouter history={history}>
+                    {StrangeRouter.buildRoutes(routes)}
+                </ConnectedRouter>
+            </Provider>
+        );
+    }
+};
+```
--- dist/index.js
+++ dist/index.js
@@ -28,6 +28,14 @@ var _extends2 = require('babel-runtime/helpers/extends');
 
 var _extends3 = _interopRequireDefault(_extends2);
 
+var _keys = require('babel-runtime/core-js/object/keys');
+
+var _keys2 = _interopRequireDefault(_keys);
+
+var _objectWithoutProperties2 = require('babel-runtime/helpers/objectWithoutProperties');
+
+var _objectWithoutProperties3 = _interopRequireDefault(_objectWithoutProperties2);
+
 var _class, _temp;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -42,7 +50,7 @@ var _require2 = require('react-router-dom/Route'),
     Route = _require2.default;
 
 var _require3 = require('react-router-dom/Redirect'),
-    Redirect = _require3.default;
+    RedirectComponent = _require3.default;
 
 var internals = {};
 
@@ -59,16 +67,39 @@ internals.renderRoute = function (basePath) {
 
     return function (route) {
 
-        var routeRedirect = route.redirect ? (0, _extends3.default)({}, route.redirect) : null;
+        if (route.redirect) {
 
-        if (routeRedirect && routeRedirect.from) {
-            routeRedirect.from = internals.concatPaths(basePath, routeRedirect.from);
-        }
+            // Redirect is special and must be exclusive of other props
+            var redirect = route.redirect,
+                rest = (0, _objectWithoutProperties3.default)(route, ['redirect']);
 
-        var redirect = routeRedirect ? React.createElement(Redirect, routeRedirect) : null;
 
-        if (!route.path && routeRedirect) {
-            return redirect;
+            if ((0, _keys2.default)(rest).length !== 0) {
+                throw new Error('No other properties are allowed alongside "redirect" in route configuration. Check childRoutes of "' + basePath + '"');
+            }
+
+            if (redirect.from) {
+
+                if (redirect.from === '/') {
+                    redirect.from = basePath;
+
+                    // For some reason, redirect.from slash paths do not prepend the entire basePath to redirect's 'to'. It cuts off the last piece so we make it absolute here
+
+                    if (!redirect.to.startsWith(basePath)) {
+                        redirect.to = internals.concatPaths(basePath, redirect.to);
+                    }
+                } else {
+                    // redirect.from should not be an absolute path, it doesn't make much sense to redirect from an absolute path in a nested route situation
+                    // Ex: inside { path: '/my/path' } there's a { redirect: { from: '/another/weird/path', to: '...' } } -- which doesn't work. 'from' will never match since the redirect won't be rendered at '/another/weird/path'
+                    // redirect.from is assumed to be relative
+
+                    if (!redirect.from.startsWith(basePath)) {
+                        redirect.from = internals.concatPaths(basePath, redirect.from);
+                    }
+                }
+            }
+
+            return React.createElement(RedirectComponent, redirect);
         }
 
         var normalizedPath = internals.concatPaths(basePath, route.path);
@@ -84,9 +115,8 @@ internals.renderRoute = function (basePath) {
                 var switcher = route.childRoutes ? React.createElement(
                     Switch,
                     null,
-                    route.childRoutes.map(internals.renderRoute(normalizedPath)),
-                    redirect
-                ) : redirect;
+                    route.childRoutes.map(internals.renderRoute(normalizedPath))
+                ) : null;
 
                 return React.createElement(
                     internals.routeComponentLifecycleWrapper,
